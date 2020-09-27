0d0635934d55ed24c0cc60ac2ff66649c7ae0443
NicolasDiazDuarte <niko476678@gmail.com>
2020-09-27 19:47:17 -0300

    Agregando datos de heroes a cada seccion

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,27 @@
+import React from "react";
+//import {Navbar} from '../components/ui/Navbar';
+
+import {LoginScreen} from '../components/login/LoginScreen'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom"
+import { DashboardRoutes } from "./DashboardRoutes";
+
+export const AppRouter = () => {
+    return (
+        <Router>
+            <div>
+
+           
+            <Switch>
+               <Route exact path="/login" component={LoginScreen} />
+               <Route path="/" component={DashboardRoutes} />
+
+            </Switch>
+        </div>
+        </Router>
+    )
+}
