fb5b89a89577fc6d3954981581c9ae2e6e7db6ae
NicolasDiazDuarte <niko476678@gmail.com>
2020-10-18 20:38:25 -0300

    busqueda de heroe por nombre

--- src/routers/DashboardRoutes.js
+++ src/routers/DashboardRoutes.js
@@ -3,6 +3,7 @@ import { Switch ,Route, Redirect} from 'react-router-dom';
 import { DcScreen } from '../components/dc/DcScreen';
 import { HeroesScreen } from '../components/heroes/HeroesScreen';
 import { MarvelScreen } from '../components/marvel/MarvelScreen';
+import { SearchScreen } from '../components/search/searchScreen';
 import { Navbar } from '../components/ui/Navbar';
 
 export const DashboardRoutes = () => {
@@ -14,8 +15,9 @@ export const DashboardRoutes = () => {
                 <Switch>
 
                     <Route exact path="/marvel" component={MarvelScreen} />  
-                    <Route exact path="/heroe/:heroeId" component={HeroesScreen} />
+                    <Route exact path="/hero/:heroeId" component={HeroesScreen} />
                     <Route exact path="/dc" component={DcScreen} />
+                    <Route exact path="/search" component={SearchScreen} />
 
                     <Redirect to="/marvel"/>
 
